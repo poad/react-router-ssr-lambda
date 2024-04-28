f064cb949decdf69354ef5a9b683d7e79be128e1
Kenji Saito <ken-yo@mbr.nifty.com>
2024-04-28 15:11:52 +0900

    Add the remix ssr by lambda

---
+++ cdk/bin/cdk.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CdkStack } from '../lib/cdk-stack';
+
+const app = new cdk.App();
+const name = 'remix-ssr-lambda-example';
+new CdkStack(app, `${name}-stack`, {
+  name,
+});
