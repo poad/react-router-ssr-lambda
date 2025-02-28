7c2f9768358ac4b560312b98ac405f74d2cc35b3
Kenji Saito <ken-yo@mbr.nifty.com>
2025-02-28 23:11:33 +0900

    Fix the errors

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,8 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ecrdeploy from 'cdk-ecr-deployment';
 import * as assets from 'aws-cdk-lib/aws-ecr-assets';
 import * as logs from 'aws-cdk-lib/aws-logs';
+import * as ecr from "aws-cdk-lib/aws-ecr";
+
 interface CdkStackProps extends cdk.StackProps {
   name: string;
 }
@@ -20,11 +23,24 @@ export class CdkStack extends cdk.Stack {
       retention: logs.RetentionDays.ONE_DAY,
     });
 
+    const repo = new ecr.Repository(this, 'Repository', {
+      repositoryName: "react-router-ssr-lambda",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteImages: true
+    });
+
+    const image = new assets.DockerImageAsset(this, 'Image', {
+      directory: '../app',
+      platform: assets.Platform.LINUX_ARM64,
+    });
+
+    new ecrdeploy.ECRDeployment(this, 'DeployDockerImage', {
+      src: new ecrdeploy.DockerImageName(image.imageUri),
+      dest: new ecrdeploy.DockerImageName(`${cdk.Aws.ACCOUNT_ID}.dkr.ecr.${cdk.Aws.REGION}.amazonaws.com/${repo.repositoryName}`),
+    })
     const handler = new lambda.DockerImageFunction(this, 'Handler', {
       functionName,
-      code: lambda.DockerImageCode.fromImageAsset('../app', {
-        platform: assets.Platform.LINUX_ARM64,
-      }),
+      code: lambda.DockerImageCode.fromEcr(repo),
       memorySize: 256,
       timeout: cdk.Duration.seconds(30),
       architecture: lambda.Architecture.ARM_64,
