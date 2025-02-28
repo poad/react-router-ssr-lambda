525e7f9a0eeb8f255d6d8dff2300ab2c3390674d
Kenji Saito <ken-yo@mbr.nifty.com>
2025-03-01 00:27:31 +0900

    Fix the errors

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,10 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as ecrdeploy from 'cdk-ecr-deployment';
 import * as assets from 'aws-cdk-lib/aws-ecr-assets';
 import * as logs from 'aws-cdk-lib/aws-logs';
 import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { Construct } from 'constructs';
 
 interface CdkStackProps extends cdk.StackProps {
   name: string;
@@ -17,7 +18,7 @@ export class CdkStack extends cdk.Stack {
     const { name } = props;
 
     const functionName = `${name}-function`;
-    new logs.LogGroup(this, 'LogGroup', {
+    const logGroup = new logs.LogGroup(this, 'LogGroup', {
       logGroupName: `/aws/lambda/${functionName}`,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       retention: logs.RetentionDays.ONE_DAY,
@@ -26,7 +27,7 @@ export class CdkStack extends cdk.Stack {
     const repo = new ecr.Repository(this, 'Repository', {
       repositoryName: "react-router-ssr-lambda",
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteImages: true
+      emptyOnDelete: true
     });
 
     const image = new assets.DockerImageAsset(this, 'Image', {
@@ -34,10 +35,40 @@ export class CdkStack extends cdk.Stack {
       platform: assets.Platform.LINUX_ARM64,
     });
 
-    new ecrdeploy.ECRDeployment(this, 'DeployDockerImage', {
+    const deployment = new ecrdeploy.ECRDeployment(this, 'DeployDockerImage', {
+      buildImage: 'public.ecr.aws/sam/build-provided.al2023:latest-arm64',
       src: new ecrdeploy.DockerImageName(image.imageUri),
-      dest: new ecrdeploy.DockerImageName(`${cdk.Aws.ACCOUNT_ID}.dkr.ecr.${cdk.Aws.REGION}.amazonaws.com/${repo.repositoryName}`),
-    })
+      dest: new ecrdeploy.DockerImageName(`${this.account}.dkr.ecr.${this.region}.amazonaws.com/${repo.repositoryName}:latest`),
+      // imageArch: ['arm64'],
+    });
+
+    const role = new iam.Role(this, 'Role', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      inlinePolicies: {
+        'InlinePolicy': new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              actions: [
+                'logs:*',
+              ],
+              resources: [
+                `${logGroup.logGroupArn}:*`,
+              ],
+            }),
+            new iam.PolicyStatement({
+              actions: [
+                'ecr:GetAuthorizationToken',
+                'ecr:BatchCheckLayerAvailability',
+                'ecr:GetDownloadUrlForLayer',
+                'ecr:BatchGetImage',
+              ],
+              resources: [repo.repositoryArn, `${repo.repositoryArn}:*`],
+            }),
+          ],
+        }),
+      }
+    });
+
     const handler = new lambda.DockerImageFunction(this, 'Handler', {
       functionName,
       code: lambda.DockerImageCode.fromEcr(repo),
@@ -49,7 +80,11 @@ export class CdkStack extends cdk.Stack {
         ASYNC_INIT: 'true',
         RUST_LOG: 'info',
       },
+      role,
     });
+
+    handler.node.addDependency(deployment);
+
     const url = handler.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
     });
